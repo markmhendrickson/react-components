be310ed38542b964e5e75e1bdd81e5c04e91fa73
Mark Hendrickson <markmhendrickson@gmail.com>
2026-01-20 13:55:18 +0100

    Export new shadcn components and hooks

--- src/index.ts
+++ src/index.ts
@@ -18,9 +18,17 @@ export { default as ErrorBoundary } from "./components/ErrorBoundary";
 // UI components
 export * from "./components/ui/breadcrumb";
 export * from "./components/ui/button";
-export * from "./components/ui/sidebar";
 export * from "./components/ui/calendar";
+export * from "./components/ui/input";
 export * from "./components/ui/popover";
+export * from "./components/ui/separator";
+export * from "./components/ui/sheet";
+export * from "./components/ui/sidebar";
+export * from "./components/ui/skeleton";
+export * from "./components/ui/tooltip";
+
+// Hooks
+export * from "./hooks/use-mobile";
 
 // Utilities
 export { cn } from "./lib/utils";
